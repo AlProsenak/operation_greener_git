37c460943e7cc807e348eae4aead669a6ab67c0b
AlProsenak <al.prosenak@gmail.com>
2024-12-08 00:57:09 +0100

    Refactor main class name

--- src/main/java/app/project/operationgreenergit/Application.java
+++ src/main/java/app/project/operationgreenergit/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OperationgreenergitApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OperationgreenergitApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
