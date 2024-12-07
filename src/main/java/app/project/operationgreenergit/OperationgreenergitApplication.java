7b27fb73c7bced4228c80a65f0da9749586da263
AlProsenak <al.prosenak@gmail.com>
2024-12-08 00:40:55 +0100

    Initialize project

---
+++ src/main/java/app/project/operationgreenergit/OperationgreenergitApplication.java
@@ -0,0 +1,13 @@
+package app.project.operationgreenergit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationgreenergitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationgreenergitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/project/operationgreenergit/OperationgreenergitApplicationTests.java
@@ -0,0 +1,13 @@
+package app.project.operationgreenergit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationgreenergitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
