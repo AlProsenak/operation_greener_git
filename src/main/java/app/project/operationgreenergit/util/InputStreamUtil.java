c5bf9595d6bffe182a7f817622b2a961a4c9b71b
AlProsenak <al.prosenak@gmail.com>
2024-12-20 19:20:10 +0100

    Add handled input stream reader

--- src/main/java/app/project/operationgreenergit/util/InputStreamUtil.java
+++ src/main/java/app/project/operationgreenergit/util/InputStreamUtil.java
@@ -1,12 +1,19 @@
 package app.project.operationgreenergit.util;
 
+import app.project.operationgreenergit.exception.ExceptionSupplier;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Assert;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import static app.project.operationgreenergit.util.MessageTemplate.EXCEPTION_CAUGHT;
 import static app.project.operationgreenergit.util.MessageTemplate.EXCEPTION_UTILITY_CLASS;
+import static app.project.operationgreenergit.util.MessageTemplate.MUST_NOT_BE_NULL;
 
+@Slf4j
 public final class InputStreamUtil {
 
 	private InputStreamUtil() {
@@ -27,6 +34,20 @@ public final class InputStreamUtil {
 		reader.close();
 
 		return sb.toString();
+	}
+
+	public static String readHandledInputStream(
+			InputStream inputStream,
+			ExceptionSupplier<? extends RuntimeException> ioExceptionSupplier) {
+		Assert.notNull(ioExceptionSupplier, MUST_NOT_BE_NULL.formatted("ioExceptionSupplier"));
+
+		try {
+			return readInputStream(inputStream);
+		} catch (IOException ex) {
+			String reason = "Failed to read input stream";
+			log.error(EXCEPTION_CAUGHT.formatted(reason), ex);
+			throw ioExceptionSupplier.get(reason, ex);
+		}
 	}
 
 }
